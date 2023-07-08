95ba642e007ea69dbfa79277afb137c19af388fe
Kerry Sabanty <kgsabanty@gmail.com>
2023-07-08 17:09:56 -0400

    arrays and exports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,48 @@
 <script>
+  // @ts-nocheck
+
   import Nested from "./Nested.svelte";
+  import Arrays from "./Arrays.svelte";
+  import PackageInfo from "./PackageInfo.svelte";
+
   let name = "Svelte";
   // let src = "/itysl1.PNG";
+  let count = 0;
+
+  $: if (count >= 10) {
+    alert("count is dangerously high");
+    count = 0;
+  }
+
+  // $ is a label to declare reactive variables
+  $: doubled = count * 2;
+
+  function increment() {
+    count += 1;
+  }
+
+  const pkg = {
+    name: "svelte",
+    speed: "blazing",
+    version: 4,
+    website: "https://svelte.dev",
+  };
 </script>
 
 <!-- <img {src} alt="itysl" -->
 
 <h1>Hello, {name.toUpperCase()}!</h1>
+<Nested answer={42} />
 <Nested />
+<Arrays />
+<PackageInfo {...pkg} />
+
+<button on:click={increment}>
+  Clicked {count}
+  {count === 1 ? "time" : "times"}
+</button>
+
+<p>{count} doubled is {doubled}</p>
 
 <style>
   h1 {
--- src/routes/Nested.svelte
+++ src/routes/Nested.svelte
@@ -1,8 +1,10 @@
 <script>
   let string = `this string contains some <strong>HTML</strong>`;
+  export let answer = "a mystery";
 </script>
 
-<p>{@html string}</p>
+<!-- <p>{@html string}</p> -->
+<p>The answer is {answer}</p>
 
 <style>
   p {
